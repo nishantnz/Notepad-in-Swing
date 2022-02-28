5f1746d64df32d5297ad1b889dd585f089116d84
nishantnz <nishant.z@somaiya.edu>
2022-02-28 21:13:35 +0530

    added manu items and shortcuts

--- src/com/nz/Notepad.java
+++ src/com/nz/Notepad.java
@@ -1,15 +1,29 @@
 package com.nz;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class Notepad extends JFrame {
     Notepad(){
         setBounds(0,0,1050,1050);
 
         JMenuBar menuBar = new JMenuBar();
+
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
         JMenu help = new JMenu("Help");
+
+        JMenuItem newFile = new JMenuItem("New");
+        newFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        JMenuItem openFile = new JMenuItem("Open");
+        openFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,ActionEvent.CTRL_MASK));
+        JMenuItem saveFile = new JMenuItem("Save");
+        saveFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+
+        file.add(newFile);
+        file.add(openFile);
+        file.add(saveFile);
 
         menuBar.add(file);
         menuBar.add(edit);
