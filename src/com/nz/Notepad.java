c62c8b271ad70cb8e3c05e3ad69e5f0f370b052f
nishantnz <nishant.z@somaiya.edu>
2022-02-28 20:04:01 +0530

    added menubar

--- src/com/nz/Notepad.java
+++ src/com/nz/Notepad.java
@@ -4,7 +4,18 @@ import javax.swing.*;
 
 public class Notepad extends JFrame {
     Notepad(){
+        setBounds(0,0,1050,1050);
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+        JMenu help = new JMenu("Help");
+
+        menuBar.add(file);
+        menuBar.add(edit);
+        menuBar.add(help);
+
+        setJMenuBar(menuBar);
     }
     public static void main(String[] args) {
        new Notepad().setVisible(true);
