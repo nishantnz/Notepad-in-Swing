acd507a3bb99f9829a529ab7f84eae7e1ea2c8a3
nishantnz <nishant.z@somaiya.edu>
2022-03-01 01:15:22 +0530

    added all the functionalities to all menuitems

--- src/com/nz/Notepad.java
+++ src/com/nz/Notepad.java
@@ -1,17 +1,24 @@
 package com.nz;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.*;
+
+import static jdk.internal.net.http.common.Utils.close;
 
 public class Notepad extends JFrame implements ActionListener {
+    JTextArea area = new JTextArea();
+    JScrollPane pane = new JScrollPane(area);
+    String text ;
+
     Notepad(){
         setBounds(0,0,1050,1050);
 
-        JTextArea area = new JTextArea();
-        JScrollPane pane = new JScrollPane(area);
+
         area.setFont(new Font("Times New Roman", Font.PLAIN, 20));
         add(pane, BorderLayout.CENTER);
         //removing border layout
@@ -96,6 +103,73 @@ public class Notepad extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("New")){
+           // new Notepad().setVisible(true);
+            area.setText("");
+        }else if(e.getActionCommand().equals("Open")){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            FileNameExtensionFilter restrict = new FileNameExtensionFilter("Only choose .txt","txt");
+            fileChooser.addChoosableFileFilter(restrict);
+            int action  = fileChooser.showOpenDialog(this);
+            if(action!=JFileChooser.APPROVE_OPTION){
+                    return;
+              }
+              File file = fileChooser.getSelectedFile();
+              BufferedReader readFileData ;
 
+                try {
+                readFileData = new BufferedReader(new FileReader(file));
+                area.read(readFileData,null);
+                setTitle(fileChooser.getSelectedFile().toString());
+                 }catch (Exception exception){
+                    area.setText(exception.toString());
+                }
+
+//           / File fileName = new File(fileChooser.setSelectedFile());
+//            //we use buffered writer to write the data...by basically saving it
+//            BufferedReader readFileData ;
+//            try {
+//                readFileData = new BufferedReader(new FileReader(fileName));
+//                area.read(readFileData);
+//            }catch (Exception exception){
+//
+//            }
+
+        }else if(e.getActionCommand().equals("Save")){
+            //choosing files using class JfileChooser
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setApproveButtonText("Save");
+            int action  = fileChooser.showOpenDialog(this);
+            if(action!=JFileChooser.APPROVE_OPTION){
+                return;
+            }
+            File fileName = new File(fileChooser.getSelectedFile()+".txt");
+            //we use buffered writer to write the data...by basically saving it
+            BufferedWriter writeFileData ;
+            try {
+                writeFileData = new BufferedWriter(new FileWriter(fileName));
+                area.write(writeFileData);
+            }catch (Exception exception){
+
+            }
+        }else if(e.getActionCommand().equals("Print")){
+            try{
+                area.print();
+            }catch (Exception exception){
+
+            }
+        }else if(e.getActionCommand().equals("Exit")){
+            System.exit(0);
+        }else if(e.getActionCommand().equals("Copy")){
+           text =area.getSelectedText();
+        }else if(e.getActionCommand().equals("Paste")){
+         area.insert(text,area.getCaretPosition());
+        }else if(e.getActionCommand().equals("Cut")){
+            text =area.getSelectedText();
+            area.replaceRange("",area.getSelectionStart(),area.getSelectionEnd());
+        }else if(e.getActionCommand().equals("Select All")){
+         area.selectAll();
+        }
     }
 }
